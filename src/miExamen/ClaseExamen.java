ee2afc6477a46f1637038d5d60f5677ec516f6b6
DamsDoesDam <ayo75987@educastur.es>
2024-02-23 19:15:57 +0100

    Añadidos comentarios de documentación en claseExamen.

--- src/miExamen/ClaseExamen.java
+++ src/miExamen/ClaseExamen.java
@@ -1,11 +1,19 @@
 
 package miExamen;
 
+
 /**
  *
- * @author alu07v
+ * @author DamianQuilezMesa
+ * @version 1.0.0
+ * @since 23/02/2024
  */
 public class ClaseExamen {
+    /**
+     * <h1>Entornos de desarrollo UT3</h1>
+     * <p>Esta clase sirve para realizar el examen de la UT3 del módulo Entornos de  Desarrollo</p>
+     *  <a href="https://www.youtube.com/playlist?list=PLU8oAlHdN5BlyaPFiNQcV0xDqy0eR35aU">Curso de Git y github de píldoras informáticas</a>
+    */
 
     /**
      * @param args the command line arguments
